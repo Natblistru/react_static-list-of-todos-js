cf97421b16ca02398f167f4ee2b374bf4c0ed2a5
Bernavschi Natalia <natb@list.ru>
2025-06-27 08:39:22 +0300

    add some fixes

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,6 +1,6 @@
 export const UserInfo = ({ user }) => (
   <div className="UserInfo">
-    <a href={`mailto: ${user.email}`} data-cy="UserInfo">
+    <a href={`mailto:${user.email}`} data-cy="UserInfo">
       {user.name}
     </a>
   </div>
